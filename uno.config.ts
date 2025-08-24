4b8c7a656c5e6edc33885c2177fab9615d38f1eb
Vladimir Sergeev <52858217+vofronte@users.noreply.github.com>
2025-08-24 09:41:44 +0700

    feat(fonts): add Geologica font and update uno config
    - Introduced Geologica font files.
    - Updated UnoCSS config to include preset-web-fonts.

--- uno.config.ts
+++ uno.config.ts
@@ -1,8 +1,30 @@
-import { defineConfig, presetAttributify, presetWind4 } from 'unocss'
+import { createLocalFontProcessor } from '@unocss/preset-web-fonts/local'
+import { createRemToPxProcessor } from '@unocss/preset-wind4/utils'
+import { defineConfig, presetAttributify, presetWebFonts, presetWind4 } from 'unocss'
 
 export default defineConfig({
   presets: [
-    presetWind4(),
+    presetWind4({
+      preflights: {
+        reset: true,
+        theme: {
+          process: createRemToPxProcessor(),
+        },
+      },
+    }),
     presetAttributify(),
+    presetWebFonts({
+      provider: 'google',
+      fonts: {
+        sans: {
+          name: 'Geologica',
+          weights: ['400', '700'],
+        },
+      },
+      processors: createLocalFontProcessor({
+        fontAssetsDir: 'public/fonts',
+      }),
+    }),
   ],
+  postprocess: [createRemToPxProcessor()],
 })
